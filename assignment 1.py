4c54413701b2b484f7407222196a36bc61e68817
basmahesham8 <45381105+basmahesham8@users.noreply.github.com>
2020-03-09 23:51:29 +0200

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,29 @@
+import sys
+print (sys.argv)
+
+type = raw_input("prompt")
+print(type) 
+#A python program to illustrate Caesar Cipher Technique 
+def shiht(text,s): 
+	result = "" 
+
+	# traverse text 
+	for i in range(len(text)): 
+		char = text[i] 
+
+		# Encrypt uppercase characters 
+		if (char.isupper()): 
+			result += chr((ord(char) + s-65) % 26 + 65) 
+
+		# Encrypt lowercase characters 
+		else: 
+			result += chr((ord(char) + s - 97) % 26 + 97) 
+
+	return result 
+
+#check the above function 
+text = "ATTACKATONCE"
+s = 4
+print "Text : " + text 
+print "Shift : " + str(s) 
+print "Cipher: " + encrypt(text,s) 
